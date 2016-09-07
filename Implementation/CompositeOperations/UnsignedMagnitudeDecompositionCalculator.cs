1fc4d07cf1ecdd4fedec655ca8dda1f3aa27cf93
Adam Furmanek <adam.furmanek@smtsoftware.com>
2016-09-07 17:17:00 +0200

    Replaced UnsignedMagnitudeDecompositionCalculator with Decomposition calculator.

--- Implementation/CompositeOperations/UnsignedMagnitudeDecompositionCalculator.cs
+++ Implementation/CompositeOperations/UnsignedMagnitudeDecompositionCalculator.cs
@@ -16,43 +16,16 @@ namespace MilpManager.Implementation.CompositeOperations
         public IEnumerable<IVariable> Calculate(IMilpManager milpManager, CompositeOperationType type, ICompositeOperationParameters parameters, params IVariable[] arguments)
         {
             if (!SupportsOperation(type, parameters, arguments)) throw new NotSupportedException(SolverUtilities.FormatUnsupportedMessage(type, parameters, arguments));
-            return InternalCalculate(milpManager, parameters, arguments);
-        }
-
-        private IEnumerable<IVariable> InternalCalculate(IMilpManager milpManager, ICompositeOperationParameters parameters, IVariable[] arguments)
-        {
+            var decomposition = milpManager.CompositeOperation(CompositeOperationType.Decomposition, new DecompositionParameters { Base = 2 }, arguments);
             if (arguments[0].IsConstant())
             {
-                int currentValue = (int)arguments[0].ConstantValue.Value;
-                for (int i = 0; i < milpManager.IntegerWidth; ++i)
-                {
-                    yield return milpManager.FromConstant(currentValue % 2);
-                    currentValue /= 2;
-                }
-                yield break;
+                return decomposition;
             }
 
-            foreach (var i in CalculateForVariable(milpManager, arguments))
+            return decomposition.Zip(Enumerable.Range(0, milpManager.IntegerWidth), (v, index) =>
             {
-                yield return i;
-            }
-        }
-
-        private static IEnumerable<IVariable> CalculateForVariable(IMilpManager milpManager, IVariable[] arguments)
-        {
-            List<Tuple<IVariable, int>> variables =
-                Enumerable.Range(0, milpManager.IntegerWidth)
-                    .Select(i => Tuple.Create(milpManager.CreateAnonymous(Domain.BinaryInteger), (int)Math.Pow(2, i)))
-                    .ToList();
-
-            milpManager.Operation(OperationType.Addition,
-                variables.Select(v => v.Item1.Operation(OperationType.Multiplication, milpManager.FromConstant(v.Item2)))
-                    .ToArray()).Set(ConstraintType.Equal, arguments[0]);
-
-            return variables.Select((v, index) => { 
-                var result = v.Item1;
-                result.Expression = $"unsignedMagnitudeDecomposition(bit: {index}, {arguments[0].FullExpression()})";
-                return result;
+                v.Expression = $"unsignedMagnitudeDecomposition(bit: {index}, {arguments[0].FullExpression()})";
+                return v;
             });
         }
     }
